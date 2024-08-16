eec942a860832e134ef09d4e02635092e54c6a37
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-08-16 19:30:44 +1000

    Got rid of Account Validator and added dependencies and shortcuts and tests

--- tests/test_Account.py
+++ tests/test_Account.py
@@ -1,11 +1,11 @@
-import pytest
 from datetime import time
 from src.account import Account
+
 
 # Tests both the struct and the getters
 class TestAccountStruct():
 
-    def test_working(self):
+    def test_getters_working(self):
         account_id = 123
         account_name = "Gmail"
         account_details = [
@@ -15,58 +15,37 @@ class TestAccountStruct():
         ]
         account_time_last_edited = time(12, 0)
 
-        account = Account(account_id, account_name, account_details, account_time_last_edited)
+        account = Account(account_id, account_name, account_details,
+                          account_time_last_edited)
 
         assert account.account_id == account_id
         assert account.account_name == account_name
         assert account.account_details == account_details
         assert account.account_time_last_edited == account_time_last_edited
 
-    def test_wrong_type_account_id(self):
-        account_id = "Hello there"
-        account_name = "Gmail"
-        account_details = [
+    def test_setters_working(self):
+        account_1 = Account(123, "Gmail", [
             "Email: bobbysmith@gmail.com",
             "Password: b0bby5m1TH",
             "Secret Question: Name of your first crush?"
-        ]
-        account_time_last_edited = time(12, 0)
+        ], time(12, 0))
 
-        with pytest.raises(TypeError):
-            account = Account(account_id, account_name, account_details, account_time_last_edited)
-        
-    def test_wrong_type_account_name(self):
-        account_id = 123
-        account_name = 123
+        account_id = 321
+        account_name = "Bank"
         account_details = [
-            "Email: bobbysmith@gmail.com",
-            "Password: b0bby5m1TH",
-            "Secret Question: Name of your first crush?"
+            "Bank Code: 1459",
+            "Email: josephbanks@outlook.com",
+            "Password: epicawesome",
+            "Secret Words: hello i have no cool"
         ]
-        account_time_last_edited = time(12, 0)
+        account_time_last_edited = time(2, 3)
 
-        with pytest.raises(TypeError):
-            account = Account(account_id, account_name, account_details, account_time_last_edited)
+        account_1.account_id = account_id
+        account_1.account_name = account_name
+        account_1.account_details = account_details
+        account_1.account_time_last_edited = account_time_last_edited
 
-    def test_wrong_type_account_details(self):
-        account_id = 123
-        account_name = "Gmail"
-        account_details = "Email: bobbysmith@gmail.com"
-        account_time_last_edited = time(12, 0)
-
-        with pytest.raises(TypeError):
-            account = Account(account_id, account_name, account_details, account_time_last_edited)
-
-    def test_wrong_type_account_time_last_edited(self):
-        account_id = 123
-        account_name = "Gmail"
-        account_details = [
-            "Email: bobbysmith@gmail.com",
-            "Password: b0bby5m1TH",
-            "Secret Question: Name of your first crush?"
-        ]
-        account_time_last_edited = "Hello"
-
-        with pytest.raises(TypeError):
-            account = Account(account_id, account_name, account_details, account_time_last_edited)
-
+        assert account_1.account_id == account_id
+        assert account_1.account_name == account_name
+        assert account_1.account_details == account_details
+        assert account_1.account_time_last_edited == account_time_last_edited
