38cd955d8ba839ad7db6b765f11c80c6d9300c50
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-08-19 22:00:54 +1000

    Finished fully implementing am_update_account

--- src/accounts_manager.py
+++ src/accounts_manager.py
@@ -59,5 +59,6 @@ class AccountsManager:
         cursor = self._conn.cursor()
         cursor.execute(f"UPDATE {TABLE_NAME} SET account_name = ? WHERE account_id = ?", (new_account_name, account_id))
 
-    def am_update_account_details(self, account_id: int, new_account_name: str):
-        return None
+    def am_update_account_details(self, account_id: int, new_account_name: List[str]):
+        cursor = self._conn.cursor()
+        cursor.execute(f"UPDATE {TABLE_NAME} SET account_details = ? WHERE account_id = ?", (json.dumps(new_account_name), account_id))
