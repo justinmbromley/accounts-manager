d1b9de71eef1472fe5e1ecf5a0d672b0d3efd87a
Justin Bromley <justinmbromley@gmail.com>
2026-05-12 08:22:59 +1000

    Created the core functions for Account service

--- src/core/AccountService.h
+++ src/core/AccountService.h
@@ -21,19 +21,28 @@ struct AccountSummary {
 enum class AddAccountError {};
 enum class UpdateAccountError {};
 enum class DeleteAccountError {};
+enum class FindAccountByIdError {};
 
 class AccountService {
 public:
+    // CREATE
     std::expected<QUuid, AddAccountError> add_account(const QString& name,
                                                       const std::vector<Credential>& credentials = {});
 
+    // RETRIEVE
+    std::optional<Account> find_account_by_id(const QUuid& id) const;
+
     std::vector<AccountSummary> find_accounts_by_name(const QString& query) const;
 
+    std::vector<Account> list_accounts() const;
+
+    // UPDATE
     std::expected<void, UpdateAccountError> update_account_name(const QUuid& id, const QString& name);
 
     std::expected<void, UpdateAccountError> update_account_credentials(const QUuid& id,
                                                                        const std::vector<Credential>& credentials);
 
+    // DELETE
     std::expected<void, DeleteAccountError> delete_account(const QUuid& id);
 };
 
