b488af29b921621e909aa02280b4c0d68c0f91b6
Justin Bromley <justinmbromley@gmail.com>
2026-05-19 11:41:50 +1000

    implemented the in memory account repository

--- src/data/InMemoryAccountRepository.cpp
+++ src/data/InMemoryAccountRepository.cpp
@@ -10,59 +10,7 @@ std::expected<void, core::CreateAccountError> InMemoryAccountRepository::add(con
     }
 
     accounts_.push_back(account);
-
-    std::expected<void, core::CreateAccountError> InMemoryAccountRepository::add(const core::Account& account) {
-        return {};
-    }
-
-    // RETRIEVE
-    std::optional<core::Account> InMemoryAccountRepository::find_by_id(const QUuid& id) const {
-        const auto it =
-            std::ranges::find_if(accounts_, [&id](const core::Account& account) { return account.id() == id; });
-
-        if (it == accounts_.cend()) {
-            return std::nullopt;
-        }
-
-        return *it;
-    }
-
-    std::vector<core::AccountSummary> InMemoryAccountRepository::find_accounts_by_name(const QString& query) const {
-        const std::vector<core::AccountSummary> summaries;
-
-        for (const auto& account : accounts_) {
-            if (account.name().contains(query, Qt::CaseInsensitive)) {
-            }
-        }
-        std::optional<core::Account> InMemoryAccountRepository::find_by_id(const QUuid& id) const {
-            return std::nullopt;
-        }
-
-        std::vector<core::AccountSummary> InMemoryAccountRepository::find_accounts_by_name(const QString& query) const {
-            return {};
-        }
-
-        std::vector<core::Account> InMemoryAccountRepository::list() const { return {}; }
-
-        // UPDATE
-        std::expected<void, core::UpdateAccountError> InMemoryAccountRepository::update(const core::Account& account) {
-            std::expected<void, core::UpdateAccountError> InMemoryAccountRepository::update(
-                const core::Account& account) {
-                return {};
-            }
-
-            // DELETE
-            std::expected<void, core::DeleteAccountError> InMemoryAccountRepository::remove(const QUuid& id) {
-                return {};
-            }
-            std::expected<void, core::DeleteAccountError> InMemoryAccountRepository::remove(const QUuid& id) {
-                return {};
-            }
-        }
-    }
-}
-
-return {};
+    return {};
 }
 
 // RETRIEVE
@@ -77,19 +25,41 @@ std::optional<core::Account> InMemoryAccountRepository::find_by_id(const QUuid& 
 }
 
 std::vector<core::AccountSummary> InMemoryAccountRepository::find_accounts_by_name(const QString& query) const {
-    const auto it = std::ranges::find_if(accounts_, [&query](const core::Account& account) {
-        return account.name().contains(query, Qt::CaseInsensitive);
-    }) return {};
+    std::vector<core::AccountSummary> summaries;
+
+    for (const auto& account : accounts_) {
+        if (account.name().contains(query, Qt::CaseInsensitive)) {
+            summaries.push_back(core::to_account_summary(account));
+        }
+    }
+
+    return summaries;
 }
 
-std::vector<core::Account> InMemoryAccountRepository::list() const { return {}; }
+std::vector<core::Account> InMemoryAccountRepository::list() const { return accounts_; }
 
 // UPDATE
 std::expected<void, core::UpdateAccountError> InMemoryAccountRepository::update(const core::Account& account) {
-    return {};
+    for (auto& stored_account : accounts_) {
+        if (stored_account.id() == account.id()) {
+            stored_account = account;
+            return {};
+        }
+    }
+
+    return std::unexpected(core::UpdateAccountError::NotFound);
 }
 
 // DELETE
-std::expected<void, core::DeleteAccountError> InMemoryAccountRepository::remove(const QUuid& id) { return {}; }
+std::expected<void, core::DeleteAccountError> InMemoryAccountRepository::remove(const QUuid& id) {
+    for (auto it = accounts_.begin(); it != accounts_.end(); ++it) {
+        if (it->id() == id) {
+            accounts_.erase(it);
+            return {};
+        }
+    }
+
+    return std::unexpected(core::DeleteAccountError::NotFound);
+}
 
 } // namespace data
